fb2ab231330ba6eb289e3c29076fb25e14fd6a47
RAKESH KUMAR <47426714+rk9155@users.noreply.github.com>
2020-07-19 00:56:19 +0530

    Basics like widget , splash screen,images,text fileds

--- hello/lib/main.dart
+++ hello/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(myApp());
 }
-
+// App1
 // Stateless widget --> cannot change at runtime
 
 // class myApp extends StatelessWidget {
@@ -70,79 +70,154 @@ void main(){
 //   }
 // }
 
+// App 2
+// // Stateful widget --> state can be changed at runtime
 
-// Stateful widget --> state can be changed at runtime
+// class myApp extends StatefulWidget {
+//   @override
+//   _myAppState createState() => _myAppState();
+// }
 
-class myApp extends StatefulWidget {
-  @override
-  _myAppState createState() => _myAppState();
-}
+// class _myAppState extends State<myApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       theme: ThemeData(
+//         primarySwatch: Colors.cyan,
+//       ),
+//       title: "Stateful",
+//       home: homePage(),
+//     );
+//   }
+// }
 
-class _myAppState extends State<myApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.cyan,
-      ),
-      title: "Stateful",
-      home: homePage(),
-    );
-  }
-}
+// class homePage extends StatefulWidget {
+//   @override
+//   _homePageState createState() => _homePageState();
+// }
 
-class homePage extends StatefulWidget {
-  @override
-  _homePageState createState() => _homePageState();
-}
+// class _homePageState extends State<homePage> {
 
-class _homePageState extends State<homePage> {
+//   int datatochange = 0;
+//   void datachange(){
+//     setState(() {
+//       datatochange+=1;
+//     });
+//   }
 
-  int datatochange = 0;
-  void datachange(){
-    setState(() {
-      datatochange+=1;
-    });
-  }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(
+//           "AppBar",
+//         )
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Text(
+//               "$datatochange",
+//             style: TextStyle(
+//               fontSize: 40.0,
+//               fontWeight: FontWeight.bold,
+//             ),
+//             ),
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "AppBar",
-        )
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "$datatochange",
-            style: TextStyle(
-              fontSize: 40.0,
-              fontWeight: FontWeight.bold,
-            ),
-            ),
+//             RaisedButton(
+//               onPressed: datachange,
+//               padding: EdgeInsets.symmetric(vertical: 15.0,horizontal: 20.0),
+//               child: Text(
+//                 "Click Me !",
+//                 style: TextStyle(
+//                 fontSize: 20.0,
+//                 color: Colors.white,
+//                 ),
+//               ),
+//               color: Colors.cyan,
+//               )
+//           ],
+//           ),
+//           ),
+//     );
+//   }
+// }
 
-            RaisedButton(
-              onPressed: datachange,
-              padding: EdgeInsets.symmetric(vertical: 15.0,horizontal: 20.0),
-              child: Text(
-                "Click Me !",
-                style: TextStyle(
-                fontSize: 20.0,
-                color: Colors.white,
-                ),
-              ),
-              color: Colors.cyan,
-              )
-          ],
-          ),
-          ),
-    );
-  }
-}
+// // App3 Using images
+
+// class myApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: Center(
+//         child: Image.asset('assets/images/emoji2.png'),
+//       ),
+//     );
+//   }
+// }
+
+// // App3 Input text field
+
+// class myApp extends StatefulWidget {
+//   @override
+//   _myAppState createState() => _myAppState();
+// }
+
+// class _myAppState extends State<myApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       title: "Textfield",
+//       home: homePage(),
+//     );
+//   }
+// }
+
+// class homePage extends StatefulWidget {
+//   @override
+//   _homePageState createState() => _homePageState();
+// }
+
+// class _homePageState extends State<homePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: <Widget>[
+//           Padding(
+//             padding: const EdgeInsets.all(10.0),
+//             child: TextField(
+//               decoration: InputDecoration(
+//                 border: OutlineInputBorder(),
+//                 hintText: "Username",
+
+//               ),
+//               maxLength: 20,
+//               autocorrect: true,
+//               autofocus: true,
+
+//               style: TextStyle(
+//                 color: Colors.red,
+//                 fontSize: 20.0,
+//               ),
+//             ),
+//           )
+
+
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// App 4 Material slpalash Screen
+
+
 
 
 
